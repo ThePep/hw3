0642b926bbf7e1e1fb72b65ae2efa231511e7936
ThePep <jesse.laux@kellogg.northwestern.edu>
2022-02-16 22:53:55 +0000

    draft 8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,16 @@
 class PostsController < ApplicationController
 
-    def index
-        @posts = Post.all 
-    end
-
     def new
         @post = Post.new
         @place = Place.find(params["place_id"])
         @post.place_id = @place.id
     end
-
+    
     def create
         @post = Post.new(params["post"])
         @post.save
-        redirect_to "/places/#{@post.place_id}"
-    end
+        redirect_to "/places/#{@post.place.id}"
+      end
+    
 
 end
